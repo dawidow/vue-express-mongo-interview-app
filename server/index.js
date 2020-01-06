8429ef103422a91b73be9e31cdd70835d91e60af
David Owczarz <owcdaw@gmail.com>
2020-01-06 21:45:24 +0100

    express app ready for deployment

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use('/api/questions', questions);
 if(process.env.NODE_ENV === 'production') {
 	app.use(express.static(__dirname + '/public/'));
 
-
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
